db269ac45851273e0c4e15c234c256fcdc3ccc52
alexweb3dev0x <alexweb3dev0x@proton.me>
2024-05-10 00:49:05 +0200

    asd

--- app/results/route.ts
+++ app/results/route.ts
@@ -1,7 +1,4 @@
-import {
-  getFrameHtml,
-  Frame,
-} from "frames.js";
+import { getFrameHtml, Frame } from "frames.js";
 import { NextRequest } from "next/server";
 import { getResults } from "../lib/db";
 
@@ -10,12 +7,12 @@ export async function POST(request: NextRequest) {
   //сам распихивай и парсь как надо
   let results = await getResults();
   let total = results[1] + results[2];
-  let res1 = (total / results[1]).toFixed(0)
-  let res2 = (total / results[2]).toFixed(0)
+  let res1 = (total / results[1]).toFixed(0);
+  let res2 = results[2] !== 0 ? (total / results[2]).toFixed(0) : 0;
   const imageUrl = `${process.env.NEXT_PUBLIC_HOST}/og?precent1=${String(
     res1
   )}%&precent2=${String(res2)}%`;
- 
+
   const frame: Frame = {
     image: imageUrl,
     version: "vNext",
