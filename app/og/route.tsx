9617de30422a9490140342b482ada7cfc3d27eb8
alexweb3dev0x <alexweb3dev0x@proton.me>
2024-05-10 20:32:02 +0200

    dzczx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -46,7 +46,7 @@ export async function GET(request: Request) {
             color: "white",
           }}
         >
-          {precent}
+          {precent}%
         </div>
         <div
           style={{
@@ -58,7 +58,7 @@ export async function GET(request: Request) {
             color: "white",
           }}
         >
-          {precent2}
+          {precent2}%
         </div>
       </div>
     ),
