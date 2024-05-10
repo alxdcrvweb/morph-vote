d58dc5078f0daaf18ea5066dec2a0fddb3c08d60
alexweb3dev0x <alexweb3dev0x@proton.me>
2024-05-10 21:03:10 +0200

    sd

--- app/results/route.ts
+++ app/results/route.ts
@@ -5,8 +5,8 @@ import { getResults } from "../lib/db";
 export async function POST(request: NextRequest) {
   let results = await getResults();
   let total = (results["1"] ? results["1"] : 0) + (results["2"] ? results["2"] : 0)
-  let res1 = results["1"] ? ((total / results["1"]) * 100).toFixed(0) : 0
-  let res2 = results["2"] ? ((total / results["2"]) * 100).toFixed(0) : 0;
+  let res1 = results["1"] ? Math.round((total / results["1"]) * 100) : 0
+  let res2 = results["2"] ? Math.round((total / results["2"]) * 100) : 0;
   const imageUrl = `${
     process.env.NEXT_PUBLIC_HOST
   }/og?precent1=${String(res1)}&precent2=${String(res2)}`;
